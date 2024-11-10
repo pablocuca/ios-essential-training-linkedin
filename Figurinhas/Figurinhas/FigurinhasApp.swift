0c0465ed193d8f2e7ec1b33900fe83cfdcb975df
Pablo C Carneiro <pablocustodio@hotmail.com>
2024-11-10 00:08:32 -0300

    new project

---
+++ Figurinhas/Figurinhas/FigurinhasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigurinhasApp.swift
+//  Figurinhas
+//
+//  Created by Pablo Custodio Carneiro on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FigurinhasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

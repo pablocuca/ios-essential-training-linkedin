017bf0abea727ba26d4fc3a0271a45c7459f4d38
Pablo C Carneiro <pablocustodio@hotmail.com>
2024-11-15 15:55:23 -0300

    Adjusting layout

--- Figurinhas/Figurinhas/ContentView.swift
+++ Figurinhas/Figurinhas/ContentView.swift
@@ -9,41 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Mingle")
                 .font(.title)
-                .fontWeight(.black)
-                .foregroundColor(Color.gray)
                 .bold()
-                .textCase(.lowercase)
-            
+                .padding()
             HStack {
                 Text("Frase: ")
                     .bold()
-                Text("Problema em dobro")
+                    .padding(.leading)
+                Text("\"Problema em dobro\"")
             }
             HStack {
                 Image("figura_01")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 100)
-                VStack {
+                    .frame(width: 75.0)
+                VStack(alignment: .leading) {
                     HStack {
                         Image(systemName: "lifepreserver")
                             .foregroundColor(Color.green)
+                            .frame(width: 30.0)
                             .font(.system(size: 30))
                         Text("Vidas:")
                         Text("2")
+                            .padding(.trailing)
                     }
                     HStack {
                         Image(systemName: "bolt")
                             .foregroundColor(Color.yellow)
+                            .frame(width: 30.0)
                             .font(.system(size: 30))
                         Text("Potência:")
                         Text("60%")
                     }
                 }
             }
+            .padding(30)
+            HStack {
+                Spacer()
+                Text("O Mingle se destaca por fazer o dobro do trabalho na metade do tempo, com extrema precisão. Essas habilidades são úteis para ela em sua função de Analista de Dados Sênior para uma empresa internacional de computação em nuvem. Ela também tem uma propensão para dança de salão, dança de linha e praticamente qualquer tipo de atividade que a deixe dançar ao som da música.")
+                Spacer()
+            }
+            Spacer()
         }
     }
 }
